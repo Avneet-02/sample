db2e540fe9ac0d2f177d62ca603c986555e17fff
Avneet-02 <kauravi1212@gmail.com>
2024-01-25 12:43:23 +0530

    messages

---
+++ Js/main.js
@@ -0,0 +1,50 @@
+// let x = 5;
+// let y = 10;
+// let sum = `The sum of $ {x} and $ {y} is $ {x + y}.`;
+// console.log(sum); 
+
+
+
+
+// let today = new Date();
+// console.log(today);
+
+// let birthday = new Date("2024-01-24");
+// console.log(birthday);
+
+
+// let date = new Date("2024-01-24");
+// console.log(date);
+
+// const d = new Date("2024-01-24T12:07:00Z");
+// console.log(d);
+
+// let name = prompt("What is your name?");
+// alert("Hello, " + name + "!");
+// 
+// alert("Hello, this is an example of a JavaScript alert!");
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+
+//   for (var i = 50; i <= 100; i++) {
+//     console.log(i);
+//   }
+
+// var id = 0;
+// var items = [];
+// for (var i = 0; i < 5; i++) {
+//   items.push({
+//     id: ++id,
+//     name: "Item " + id
+//   });
+// }
+
+// let name = "John"; 
+// let greeting = "Hello, " + name;
+// console.log(greeting);
+
+// let user = "Avneet";
+// let username = "Hey!, " + user;
+// console.log(username);
